82fcab90db0797c5b2a565589860dcb5fa7d8c6e
Cik-hey <christian.karl.calano@oocl.com>
2022-09-01 14:26:40 +0800

    feat: added 3 & 5 & 7 to condition in FizzBuzz(main)

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,6 +2,9 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
     public String countOff(int order) {
+        if(order % 3 == 0 && order % 5 == 0 && order % 7 == 0){
+            return "FizzBuzzWhizz";
+        }
         if(order % 3 == 0 && order % 5 == 0){
             return "FizzBuzz";
         }
