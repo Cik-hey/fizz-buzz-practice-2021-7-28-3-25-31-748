0c4792def4e4b7a82e86bea12cf48ade25e738c3
Cik-hey <christian.karl.calano@oocl.com>
2022-09-01 14:45:18 +0800

    refactor: Extract Constant refactoring in FizzBuzz

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,27 +1,36 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
+
+    public static final String WHIZZ = "Whizz";
+    public static final String BUZZ = "Buzz";
+    public static final String FIZZ = "Fizz";
+    public static final String FIZZ_WHIZZ = "FizzWhizz";
+    public static final String BUZZ_WHIZZ = "BuzzWhizz";
+    public static final String FIZZ_BUZZ = "FizzBuzz";
+    public static final String FIZZ_BUZZ_WHIZZ = "FizzBuzzWhizz";
+
     public String countOff(int order) {
         if(order % 3 == 0 && order % 5 == 0 && order % 7 == 0){
-            return "FizzBuzzWhizz";
+            return FIZZ_BUZZ_WHIZZ;
         }
         if(order % 3 == 0 && order % 5 == 0){
-            return "FizzBuzz";
+            return FIZZ_BUZZ;
         }
         if(order % 5 == 0 && order % 7 == 0){
-            return "BuzzWhizz";
+            return BUZZ_WHIZZ;
         }
         if(order % 3 == 0 && order % 7 == 0){
-            return "FizzWhizz";
+            return FIZZ_WHIZZ;
         }
         if(order % 3 == 0){
-            return "Fizz";
+            return FIZZ;
         }
         if(order % 5 == 0){
-            return "Buzz";
+            return BUZZ;
         }
         if(order % 7 == 0){
-            return "Whizz";
+            return WHIZZ;
         }
         return String.valueOf(order);
     }
