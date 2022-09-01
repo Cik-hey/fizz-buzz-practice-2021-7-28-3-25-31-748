1f384a7ce82a57a7391d4b343cf2ef43d854d3af
Cik-hey <christian.karl.calano@oocl.com>
2022-09-01 14:04:11 +0800

    feat: added test case

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -7,12 +7,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class FizzBuzzTest {
 
     @Test
-    void should_return_1_when_when_count_off_given_number_1() {
+    void should_return_fizzBuzzWhizz_when_when_count_off_given_number() {
+        //given
         FizzBuzz fizzBuzz = new FizzBuzz();
         String expected = "1";
 
+        //when
         String actual = fizzBuzz.countOff(1);
 
+        //then
         assertEquals(expected, actual);
     }
 }
